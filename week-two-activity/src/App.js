4e6e953b0baf254dfca0ce6f609c14f201fbc22d
OrlandoGonzalez <orlando_garcia_glez@hotmail.com>
2022-03-06 12:03:53 -0600

    Finalización de la actividad de la semana 2.

--- week-two-activity/src/App.js
+++ week-two-activity/src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
+import './components/style_pokemon.css';
+import React, {useState} from 'react';
 import Pokemon from './components/Pokemon';
-const pikachu_img = require("./img/pikachu.png");
+const pikachu = require("./img/pikachu.gif");
+const charizard = require("./img/charizard.gif");
+const umbreon = require("./img/umbreon.gif");
+const blastoise = require("./img/blastoise.gif");
 const img_404 = require("./img/404.png");
+var pokemones = ["PIKACHU", "BLASTOISE", "CHARIZARD", "UMBREON"];
+var pathPokemones = [pikachu, blastoise, charizard, umbreon];
+
+function random(min, max) {
+  return Math.floor((Math.random() * (max - min + 1)) + min);
+}
 
 function App() {
+  const [pokemon, setPokemon] = useState(
+    {
+      name: pokemones[0],
+      path: pathPokemones[0] 
+    }
+  );
+  const changePokemon = () =>{
+    let indexPokemon = random(0, 3);
+    setPokemon({...pokemon, name: pokemones[indexPokemon], path: pathPokemones[indexPokemon]});
+    console.log(pokemon);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <Pokemon name='Pikachu' path={pikachu_img} alt={img_404} />
+        <Pokemon name={pokemon.name} path={pokemon.path} alt={img_404} /><br/>
+        <button className='button' onClick={() => changePokemon()}>Cambiar Pokemón</button>
       </header>
     </div>
   );
--- week-two-activity/src/components/Pokemon.jsx
+++ week-two-activity/src/components/Pokemon.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import './style_pokemon.css';
 
 function Pokemon(props){
     return (
         <>
-            <h1>Pokemón: {props.name}</h1>
-            <img src={props.path} alt={props.alt} width="300" height="300"></img>
+            <h1 className='title'>{props.name}</h1>
+            <img src={props.path} alt={props.alt} width="500" className='image'></img>
         </>
     );
-}
+} 
 
 export default Pokemon;
